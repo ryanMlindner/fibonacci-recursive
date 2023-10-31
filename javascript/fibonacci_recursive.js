1fdf10c25b8ff27102618dbf1fcc9c3347533a18
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-10-31 05:45:19 -0600

    pseudocode

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -21,4 +21,21 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+/**
+ * given an integer n
+ * if n = 0 or n = 1 : value = n
+ * else value of that number of the fibonacci sequence equals the previous two numbers added
+ * call recursively to determine values of previous numbers
+ */
 // And a written explanation of your solution
+/**
+ * the first two numbers of the sequence are 0 and 1
+ * after that they are derived by adding the previous two values
+ * turning an iterative version into recursive involves 
+ * switching around how the values are derived
+ * going from the end back to the beginning
+ * rather than building it up as you go in iteration
+ * the formula gets much easier to understand
+ * the drawback being that the bigger the integer
+ * the more likely the recursion will break memory
+ */
